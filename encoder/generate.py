9bfa0ba60f540931e7599820e47c833f3eebe8ec
thomas-emb <thomas_de_bont@hotmail.com>
2024-10-11 14:10:23 +0200

    fully parametrized generation of encoders

--- encoder/generate.py
+++ encoder/generate.py
@@ -1,11 +1,9 @@
 import uuid
 from sexpdata import Symbol, dumps as SExpDump # pip install sexpdata
 import re
-import cmath
+from cmath import rect, phase, pi
+from pathlib import Path
 
-symbolName = "year"
-
-unlocked = [Symbol("unlocked"), True]
 
 def property(name, value, position, layer="F.Fab", hide=None, size = [1, 1], thickness = 0.15, property_name="property"):
     property = [Symbol(property_name), name, str(value),
@@ -22,18 +20,18 @@ def property(name, value, position, layer="F.Fab", hide=None, size = [1, 1], thi
         property.insert(6, [Symbol("hide"), hide])
     return property
 
-def deg(degrees):
-    return degrees/360 * cmath.pi * 2
+def rad(degrees):
+    return degrees/360 * 2*pi
 
-def polygon(inner_radius, outer_radius, center_angle, angular_width):
-    inner = [
-        cmath.rect(inner_radius, deg(center_angle - angular_width/2)),
-        cmath.rect(inner_radius, deg(center_angle)),
-        cmath.rect(inner_radius, deg(center_angle + angular_width/2))]
-    outer = [
-        cmath.rect(outer_radius, deg(center_angle + angular_width/2)),
-        cmath.rect(outer_radius, deg(center_angle)),
-        cmath.rect(outer_radius, deg(center_angle - angular_width/2))]
+def polygon(innerRadius, outerRadius, centerAngle, angularWidth, spacing=0):
+    angles = [centerAngle - angularWidth/2, centerAngle, centerAngle + angularWidth/2]
+    inner = [rect(innerRadius, rad(angles[0])), rect(innerRadius, rad(angles[1])), rect(innerRadius, rad(angles[2]))]
+    outer = [rect(outerRadius, rad(angles[2])), rect(outerRadius, rad(angles[1])), rect(outerRadius, rad(angles[0]))]
+    if spacing != 0: # spacing is not exact, but good enough for small spacings
+        inner[0] = rect(innerRadius, phase(inner[0] + rect(spacing, rad(angles[0] + 90))))
+        outer[2] = rect(outerRadius, phase(outer[2] + rect(spacing, rad(angles[0] + 90))))
+        inner[2] = rect(innerRadius, phase(inner[2] + rect(spacing, rad(angles[2] - 90))))
+        outer[0] = rect(outerRadius, phase(outer[0] + rect(spacing, rad(angles[2] - 90))))
     return [Symbol("fp_poly"),
         [Symbol("pts"),
             [Symbol("arc"),
@@ -56,9 +54,9 @@ def polygon(inner_radius, outer_radius, center_angle, angular_width):
         [Symbol("uuid"), str(uuid.uuid4())]
     ]
 
-def pad(hole, size):
+def clearancePad(hole, size):
     return [
-    Symbol("pad"), "", Symbol("thru_hole"), Symbol("circle"),
+    Symbol("pad"), "", Symbol("np_thru_hole"), Symbol("circle"),
         [Symbol("at"), 0, 0],
         [Symbol("size"), size, size],
         [Symbol("drill"), hole],
@@ -67,33 +65,93 @@ def pad(hole, size):
         [Symbol("uuid"), str(uuid.uuid4())]
     ]
 
-def items(hole, size, margin, spacing):
-    result = [
-    property("Reference", "P**", [7, -6.5, 0], "F.SilkS", thickness=0.1),
-    property("Value", symbolName, [0, 1, 0]),
-    property("Footprint", "", [0, 0, 0], hide=True),
-    property("Datasheet", "", [0, 0, 0], hide=True),
-    property("Description", "", [0, 0, 0], hide=True),
-    [Symbol("attr"), Symbol("smd")]]
-    result.append(polygon(5.16, size/2 - margin, -90, 266))
-    result.append(polygon(5, size/2 - margin,  90,  90))
-    result.append(polygon(hole/2 + margin, 5, 0, 180))
-    result.append(polygon(3, 5, 180, 180))
+class Slice:
+    def __init__(self, center, width, space=False, goIn=False, goOut=False):
+        self.center = center
+        self.width = width
+        self.space = space
+        self.goIn = goIn
+        self.goOut = goOut
 
+def Circle():
+    return [Slice(0, 180), Slice(180, 180)]
+
+def createFootprint(name, hole, size, margin, spacing, rings):
+    innerRadius = hole/2 + (margin - spacing/2)
+    outerRadius = size/2 - (margin - spacing/2)
+    ringHeight = (outerRadius - innerRadius)/len(rings)
+    radii = [ring*ringHeight + innerRadius for ring in range(len(rings) + 1)]
+    result = [
+        property("Reference", "P**", [size/2, -size/2, 0], "F.SilkS", thickness=0.1),
+        property("Value", name, [size/2, size/2, 0]),
+        property("Footprint", "", [0, 0, 0], hide=True),
+        property("Datasheet", "", [0, 0, 0], hide=True),
+        property("Description", "", [0, 0, 0], hide=True),
+        [Symbol("attr"), Symbol("smd")]
+    ]
+    for ring, slices in enumerate(rings):
+        for slice in slices:
+            result.append(polygon(
+                radii[ring] + (-spacing if slice.goIn else spacing/2),
+                radii[ring+1] - (-spacing if slice.goOut else spacing/2),
+                slice.center,
+                slice.width,
+                spacing if slice.space else 0
+            ))
     kicad_mod = [
-        Symbol("footprint"), symbolName,
+        Symbol("footprint"), name,
         [Symbol("version"), 20240108],
         [Symbol("generator"), "python"],
         [Symbol("generator_version"), "0.1"],
         [Symbol("layer"), "F.Cu"]
     ] + result + [
-        property(Symbol("user"), "${REFERENCE}", [0, 4, 0], property_name="fp_text"),
-        pad(hole, size)
+        property(Symbol("user"), "${REFERENCE}", [-size/2, size/2, 0], property_name="fp_text"),
+        clearancePad(hole, size)
     ]
-
     kicad_mod = SExpDump(kicad_mod, true_as="yes", false_as="no", pretty_print=True, indent_as="\t")
     kicad_mod = re.sub("\n[\t]+(?=[^\t()])", " ", kicad_mod)
     kicad_mod = re.sub("\\( ", "(", kicad_mod)
     return kicad_mod
 
-print(items(5, 15, 0.5, 0.15))
+def writeEncoder(name, slices):
+    Path("../../msp_clock/msp_clock/date clock encoders.pretty/"+name+".kicad_mod").write_text(
+        createFootprint(name, 5, 15, 0.5, 0.15, slices)
+    )
+
+writeEncoder("day", [
+    [Slice(6, 18, goOut=True), Slice(186, 342, space=True)], # day 29-31 and space after
+    Circle(),
+    [Slice(93, 180, goIn=True), Slice(273, 180, space=True)] # day 1-30 and space after
+])
+
+writeEncoder("week", [Circle(),
+    [
+        Slice(234, 6, goIn=True), Slice(240, 6, space=True), # 1st day and space after
+        Slice(246, 6, goIn=True), Slice(252, 6, space=True), # 2nd
+        Slice(258, 6, goIn=True), Slice(264, 6, space=True), # 3rd
+        Slice(270, 6, goIn=True), Slice(276, 6, space=True), # 4th
+        Slice(282, 6, goIn=True), Slice(288, 6, space=True), # 5th
+        Slice(294, 6, goIn=True), Slice(300, 6, space=True), # 6th
+        Slice(306, 6, goIn=True), Slice( 90, 282, space=True)# 7th day and remaining space
+    ]
+])
+
+writeEncoder("month", [Circle(),
+    [
+        Slice(  0, 15, goIn =True), Slice( 15, 15, space=True), # September
+        Slice( 30, 15, goOut=True), Slice( 45, 15, space=True), # August
+        Slice( 60, 15, goOut=True), Slice( 75, 15, space=True), # July
+        Slice( 90, 15, goIn =True), Slice(105, 15, space=True), # June
+        Slice(120, 15, goOut=True), Slice(135, 15, space=True), # May
+        Slice(150, 15, goIn =True), Slice(165, 15, space=True), # April
+        Slice(180, 15, goOut=True),#Slice(195, 15, space=True), # March
+        Slice(210, 45, space=True),#Slice(225, 15, space=True), # February
+        Slice(240, 15, goOut=True), Slice(255, 15, space=True), # January
+        Slice(270, 15, goOut=True), Slice(285, 15, space=True), # December
+        Slice(300, 15, goIn =True), Slice(315, 15, space=True), # November
+        Slice(330, 15, goOut=True), Slice(345, 15, space=True), # October
+    ],
+    Circle()
+])
+
+writeEncoder("year", [Circle(), [Slice(90, 90, goIn=True), Slice(-90, 270, space=True)]])
